494b3ab12d3b685921e234d78d516bfbd893af95
Vishruth <vishruthvs2003@gmail.com>
2024-03-18 01:28:29 +0530

    deploying

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -14,13 +14,20 @@ export const blogRouter=new  Hono<{
 blogRouter.use("/*",async(c,next)=>{
     const authHeader=c.req.header('authorization')||"";
     const user=await verify(authHeader,c.env.JWT_SECRET);
-    if(user){
+    try{
+          if(user){
         c.set("userId",user.id);
     await  next();
     }else{
         c.status(403);
         return c.json({error:"You are not logged in"})
     }
+    }catch(e){
+        console.log(e);
+        c.status(403);
+        return c.json({error:"You are not logged in"})
+    }
+  
 
 })
 
