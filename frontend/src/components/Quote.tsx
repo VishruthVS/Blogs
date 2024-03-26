bd266f58f8908e16125b7cd5ff63bf19fd404065
VishruthVS <vishruthvs2003@gmail.com>
2024-03-26 20:34:13 +0530

    frontend basic working

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -7,7 +7,7 @@ export const Quote = () => {
                     "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                    Julies Winfield || Vishruth VS
                 </div>
                 <div className="max-w-md text-sm font-light text-slate-400">
                     CEO | Acme corp
