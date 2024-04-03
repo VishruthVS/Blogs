b07219355178f1b41b1c7ec234fee99b5212b583
Srushti S <srushhhti07@gmail.com>
2024-04-03 21:35:22 +0530

    home pages changes done

---
+++ frontend/src/components/Sections/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className="hero-section bg-gradient-to-r from-pink-900 to-indigo-100 text-white py-12 px-4 sm:px-6 lg:px-8 h-screen flex justify-center items-center">
+            <div className="container mx-auto text-center">
+                <h1 className="text-4xl lg:text-6xl font-bold mb-4">Curiosity spark chronicles</h1>
+                <p className="text-lg lg:text-xl">Explore ideas, spark conversations: Your gateway to engaging content</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,20 +5,25 @@ import { SecComponent } from "../components/Sections/SecComponent"
 import { ThirdComponent } from "../components/Sections/ThirdComponent"
 import { FourthComponent } from "../components/Sections/FourthComponent"
 import { FifthComponent } from "../components/Sections/FifthComponent"
+import HeroSection from "../components/Sections/HeroSection"
 
 export const Home = () => {
     return (
         <div>
-            <header className="flex justify-between items-center py-4 px-8">
+            <header className="fixed top-0 left-0 right-0 z-50 bg-white flex justify-between items-center py-4 px-8">
                 <div className="font-serif font-semibold text-[30px] lg:text-[44px] hover:italic pl-3">
                     <Link to="/">Medium</Link>
                 </div>
                 <div className="font-serif font-medium text-lg pr-3">
-                <Link to={'/signin'} >
-                    Log in ~
-                </Link>
+                    <Link to={'/signin'} className="font-bold mr-4">
+                        Log in
+                    </Link>
+                    <Link to={'/signup'} className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                        Get Started
+                    </Link>
                 </div>
             </header>
+            <HeroSection/>
             <div className="flex justify-between items-center bg-sky-50 py-4 px-8">
                 <div className="font-lg text-gray-800">
                     CATEGORIES
